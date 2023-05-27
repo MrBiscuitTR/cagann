e7b643a62e05f1586f0e3089832b7dda877c7a1e
MrBiscuitTR <efecalidag@gmail.com>
2023-05-27 21:54:18 +0300

    nope I wanna use react now

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={images} className="App-logo" alt="logo" /> */}
+        <p>
+          hey
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
