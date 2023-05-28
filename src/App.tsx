26a77daa2fd1b8e46c79ae1873288f83db804fa3
MrBiscuitTR <efecalidag@gmail.com>
2023-05-28 16:16:23 +0300

    fresh

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <h1>Page Under Construction</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis expedita hic error ratione nostrum ullam, omnis soluta maxime velit quia, ipsam odit ab maiores consequatur a non eligendi optio libero.
+            </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
