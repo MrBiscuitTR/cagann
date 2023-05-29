8fecd9e633d106c8c028baca3a97144a8937bc9f
MrBiscuitTR <efecalidag@gmail.com>
2023-05-29 15:56:35 +0300

    cname

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,6 @@ function App() {
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis expedita hic error ratione nostrum ullam, omnis soluta maxime velit quia, ipsam odit ab maiores consequatur a non eligendi optio libero.
             </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
