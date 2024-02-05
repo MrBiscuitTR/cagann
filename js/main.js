a99f983cbb6b404dca707434a2c04a7b82ad157a
MrBiscuitTR <efecalidag@gmail.com>
2024-02-05 23:05:25 +0300

    test server (remove later)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 //server request template test
 const rq = new XMLHttpRequest();
 function serverRequest() {
-  rq.open("GET", "http://localhost:8080/");
+  rq.open("GET", "http://92.44.187.183:80");
   rq.send();
     rq.onload = () => {
         if (rq.status === 200) {
         console.log(rq.responseText);
+        const data = JSON.parse(rq.responseText);
+        const aboutdiv = document.getElementById("about");
+        aboutdiv.appendChild(document.createTextNode(data.name));
         } else {
         console.log("Error: " + rq.status);
         }
